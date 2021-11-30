cfc7cb6ce11349c1351de577948a8462f2a9c354
1nt18is039bharat <81946492+1nt18is039bharat@users.noreply.github.com>
2021-11-30 15:26:03 +0530

    Add files via upload

---
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.lv1);
+        String values[] = getResources().getStringArray(R.array.subjects);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arrays.asList(values));
+        lv.setAdapter(ad);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "clicked the item:"+values[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
